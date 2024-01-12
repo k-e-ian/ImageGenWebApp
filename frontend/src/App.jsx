6cdaffb1e1f917daa691428524257dcecf11410e
ïanKitembe <anedwin@outlook.com>
2024-01-12 23:26:06 +0300

    edit api endpoint to deployed backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,11 @@
-import React, { useState } from 'react';
-import request from 'superagent';
-import { BounceLoader } from 'react-spinners';
-import Header from './components/Header';
-import Footer from './components/Footer';
+import React, { useState } from "react";
+import request from "superagent";
+import { BounceLoader } from "react-spinners";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
-  const [prompt, setPrompt] = useState('');
+  const [prompt, setPrompt] = useState("");
   const [generatedImage, setGeneratedImage] = useState(null);
   const [imageId, setImageId] = useState(null);
   const [loading, setLoading] = useState(false);
@@ -17,30 +17,29 @@ function App() {
       setError(null);
 
       const response = await request
-        .post('http://localhost:3000/generate')
+        .post("https://dall-e-2-or6h.onrender.com/generate")
         .send({ prompt: prompt })
         .timeout({
-          response: 15000,  // Wait for the server to start sending
-          deadline: 20000,  // Entire operation must finish within 15 seconds
+          response: 15000, // Wait for the server to start sending
+          deadline: 20000, // Entire operation must finish within 15 seconds
         });
 
       const imageData = response.body.data[0];
       setGeneratedImage(imageData.url);
       setImageId(response.body.created); // Use 'created' as the image ID
     } catch (error) {
-      console.error('Error generating image:', error);
-      setError('Error generating image. Please try again.'); // Provide a user-friendly error message
+      console.error("Error generating image:", error);
+      setError("Error generating image. Please try again."); // Provide a user-friendly error message
     } finally {
       setLoading(false);
     }
   };
 
   const handleKeyPress = (e) => {
-    if (e.key === 'Enter') {
+    if (e.key === "Enter") {
       handleGenerateImage();
     }
   };
-
 
   return (
     <>
@@ -49,33 +48,35 @@ function App() {
         <h1>Image Generator</h1>
         <div className='img-gen-input'>
           <input
-            type="text"
-            placeholder="Enter prompt"
+            type='text'
+            placeholder='Enter prompt'
             value={prompt}
             onChange={(e) => setPrompt(e.target.value)}
             onKeyPress={handleKeyPress}
           />
-          <button className="button" onClick={handleGenerateImage}>
+          <button className='button' onClick={handleGenerateImage}>
             Generate
           </button>
         </div>
       </div>
 
       {loading && (
-        <div className="loading-spinner">
+        <div className='loading-spinner'>
           <BounceLoader loading={loading} size={50} />
         </div>
       )}
       {error && (
-        <div className="loading-spinner">
-          <p className="error">{error}</p>
+        <div className='loading-spinner'>
+          <p className='error'>{error}</p>
         </div>
       )}
 
       {generatedImage && (
         <div className='image-div'>
           <div className='image-name'>
-            <code>Generated image id: <span className='image-id'>{imageId}</span></code>
+            <code>
+              Generated image id: <span className='image-id'>{imageId}</span>
+            </code>
           </div>
           <div className='image'>
             <img
@@ -85,19 +86,21 @@ function App() {
             />
           </div>
           <div className='image-url'>
-          <code className='image-url'>
-              Generated image URL:{' '}
-            <a
-              href={generatedImage}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="image-link"
-            >
-              <span>
-                {generatedImage.length > 40 ? generatedImage.slice(0, 40) + '...' : generatedImage}
-              </span>
-            </a>
-          </code>
+            <code className='image-url'>
+              Generated image URL:{" "}
+              <a
+                href={generatedImage}
+                target='_blank'
+                rel='noopener noreferrer'
+                className='image-link'
+              >
+                <span>
+                  {generatedImage.length > 40
+                    ? generatedImage.slice(0, 40) + "..."
+                    : generatedImage}
+                </span>
+              </a>
+            </code>
           </div>
         </div>
       )}
